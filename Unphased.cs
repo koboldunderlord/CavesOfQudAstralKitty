61c6a676c60555f22aa8949d2b774b62ef5b04d6
Crom <james.vogon@gmail.com>
2019-06-16 08:23:30 -0700

    Init from 2019-06-07 Workshop Release

---
+++ Unphased.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using XRL.Core;
+using XRL.Language;
+using XRL.Rules;
+using XRL.World.Capabilities;
+
+namespace XRL.World.Parts.Effects
+{
+    [Serializable]
+    public class Unphased : Effect
+    {
+        private static System.Type EffectUnphased = ModManager.ResolveType(typeof(Unphased).FullName);
+        public string RenderString = "a";
+        public string Tile;
+
+        public Unphased()
+        {
+        }
+
+        public Unphased(int _Duration)
+          : this()
+        {
+            this.Duration = _Duration;
+        }
+
+        public override bool SameAs(Effect e)
+        {
+            Unphased unphased = e as Unphased;
+            if (unphased.Tile != this.Tile || unphased.RenderString != this.RenderString)
+                return false;
+            return base.SameAs(e);
+        }
+
+        public override string GetDetails()
+        {
+            return "Temporarily able to interact with creatures and objects unless they're phased.\nCan no longer pass through solids.";
+        }
+
+        public override bool Apply(GameObject Object)
+        {
+            Object.RemoveEffect("Phased", false);
+            return true;
+        }
+
+        public override void Remove(GameObject Object)
+        {
+            Object.ApplyEffect((Effect)new Phased(9999));
+        }
+
+        public override void Register(GameObject Object)
+        {
+            this.Tile = Object.pRender.Tile;
+            this.RenderString = Object.pRender.RenderString;
+            Object.RegisterEffectEvent((Effect)this, "EndTurn");
+            base.Register(Object);
+        }
+
+        public override void Unregister(GameObject Object)
+        {
+            Object.UnregisterEffectEvent((Effect)this, "EndTurn");
+        }
+
+        public override bool Render(RenderEvent E)
+        {
+            return true;
+        }
+
+        public override bool FireEvent(Event E)
+        {
+            if (E.ID == "EndTurn" && this.Duration > 0)
+            {
+                if (this.Duration > 1 && this.Object.IsPlayer() && this.Duration != 9999)
+                    Effect.AddPlayerMessage("You will phase back out in " + Grammar.Cardinal(this.Duration - 1) + " " + (this.Duration - 1 != 1 ? "turns" : "turn") + ".");
+                if (this.Duration != 9999)
+                    --this.Duration;
+            }
+            return base.FireEvent(E);
+        }
+    }
+}
---
+++ Unphasing.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using XRL.World.AI.GoalHandlers;
+using XRL.World.Parts.Effects;
+
+namespace XRL.World.Parts.Mutation
+{
+    [Serializable]
+    public class Unphasing : BaseMutation
+    {
+        public Guid UnphaseActivatedAbilityID = Guid.Empty;
+        public ActivatedAbilityEntry UnphaseActivatedAbility;
+
+        public Unphasing()
+        {
+            this.DisplayName = "Astral Walker";
+        }
+
+        public override void Register(GameObject Object)
+        {
+            Object.RegisterPartEvent((IPart)this, "CommandUnphase");
+            Object.RegisterPartEvent((IPart)this, "AIGetOffensiveMutationList");
+            base.Register(Object);
+        }
+
+        public override string GetDescription()
+        {
+            return "You live in an alternate reality, but may phase into reality for brief periods of time.";
+        }
+
+        public override string GetLevelText(int Level)
+        {
+            return "Cooldown: " + (object)(103 - 3 * Level) + " rounds\n" + "Duration: " + (object)(6 + Level) + " rounds";
+        }
+
+        public override bool FireEvent(Event E)
+        {
+            if ((E.ID == "BeginTakeAction" || E.ID == "EnteredCespinll") && (!this.ParentObject.HasEffect("Phased") && !this.ParentObject.HasEffect("RealityStabilized") && !this.ParentObject.HasEffect("Unphased")))
+            {
+                this.ParentObject.ApplyEffect((Effect)new Phased(9999));
+                return true;
+            }
+            if (E.ID == "CommandUnphase")
+            {
+                if (!this.ParentObject.FireEvent(Event.New("InitiateRealityDistortionLocal", "Object", (object)this.ParentObject, "Mutation", (object)this), E))
+                    return true;
+                this.ParentObject.ApplyEffect((Effect)new Unphased(6 + this.Level + 1));
+                this.UnphaseActivatedAbility.Cooldown = 1040 - 30 * this.Level;
+            }
+            if (!(E.ID == "AIGetOffensiveMutationList"))
+                return base.FireEvent(E);
+            int parameter1 = (int)E.GetParameter("Distance");
+            List<AICommandList> parameter2 = (List<AICommandList>)E.GetParameter("List");
+            if (this.UnphaseActivatedAbility != null && this.UnphaseActivatedAbility.Cooldown <= 0 && (parameter1 > 1 && parameter1 < 6 + this.Level - 1) && this.ParentObject.FireEvent(Event.New("CheckRealityDistortionAdvisability", "Mutation", (object)this)))
+                parameter2.Add(new AICommandList("CommandPhase", 1));
+            return true;
+        }
+
+        public override bool ChangeLevel(int NewLevel)
+        {
+            return base.ChangeLevel(NewLevel);
+        }
+
+        public override bool Mutate(GameObject GO, int Level)
+        {
+            this.ParentObject.ApplyEffect((Effect)new Phased(9999));
+            ActivatedAbilities part = GO.GetPart("ActivatedAbilities") as ActivatedAbilities;
+            if (part != null)
+            {
+                this.UnphaseActivatedAbilityID = part.AddAbility("Unphase", "CommandUnphase", "Physical Mutation", true, false);
+                this.UnphaseActivatedAbility = part.AbilityByGuid[this.UnphaseActivatedAbilityID];
+            }
+            this.ChangeLevel(Level);
+            return base.Mutate(GO, Level);
+        }
+
+        public override bool Unmutate(GameObject GO)
+        {
+            if (this.UnphaseActivatedAbilityID != Guid.Empty)
+            {
+                (GO.GetPart("ActivatedAbilities") as ActivatedAbilities).RemoveAbility(this.UnphaseActivatedAbilityID);
+                this.UnphaseActivatedAbilityID = Guid.Empty;
+            }
+            return base.Unmutate(GO);
+        }
+    }
+}
